3ce289ab904530dda456672ceecf68a786bf8093
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2022-07-05 16:23:29 +0200

    Initial commit

---
+++ src/main/java/com/example/efinancebooking/EFinanceBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.efinancebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFinanceBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EFinanceBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/efinancebooking/EFinanceBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.efinancebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFinanceBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
