f713ba66aad28053954580104d37ef1e8086d48f
ahmedssameh <59616470+ahmedssameh@users.noreply.github.com>
2022-07-20 16:42:50 +0200

    Product List but still not sortable

---
+++ src/main/java/com/example/efinancebooking/Controller/ProductListController.java
@@ -0,0 +1,36 @@
+package com.example.efinancebooking.Controller;
+
+import com.example.efinancebooking.Model.BookingObject;
+import com.example.efinancebooking.Repos.BookingObjectRepo;
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.annotation.RequestAction;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.ocpsoft.rewrite.faces.annotation.Deferred;
+import org.ocpsoft.rewrite.faces.annotation.IgnorePostback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Scope (value = "session")
+@Component (value = "productList")
+@ELBeanName(value = "productList")
+@Join(path = "/", to = "/product-list.jsf")
+public class ProductListController {
+    @Autowired
+    private BookingObjectRepo bookingObjectRepo;
+
+    private List<BookingObject> products;
+
+    @Deferred
+    @RequestAction
+    @IgnorePostback
+    public void loadData() {
+        products = bookingObjectRepo.findAll();
+    }
+
+    public List<BookingObject> getProducts() {
+        return products;
+    }
+}
