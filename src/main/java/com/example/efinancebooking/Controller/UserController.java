528e95bfc53363330ee9377f553ab87351b9fce0
abdelrahman <a.elrahman.hussain@gmail.com>
2022-07-27 13:56:32 +0200

    signup

--- src/main/java/com/example/efinancebooking/Controller/UserController.java
+++ src/main/java/com/example/efinancebooking/Controller/UserController.java
@@ -5,7 +5,10 @@ import com.example.efinancebooking.Model.User;
 import com.example.efinancebooking.Services.UserServices;
 import com.example.efinancebooking.UserRequests.AddUserRequest;
 import lombok.Data;
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.el.ELBeanName;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +22,10 @@ import java.util.List;
 @RequestMapping(path="/User")
 @Component(value = "UserController")
 @Data
+
+@ELBeanName(value = "signUp")
+@Scope(value = "session")
+@Join(path = "/signUp", to = "/signUp.jsf")
 public class UserController {
 
     @Autowired
